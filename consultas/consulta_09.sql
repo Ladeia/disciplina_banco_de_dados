b3cb529908064375c472330834422fc15717cda6
GeovaneOB <118844613+GeovaneOB@users.noreply.github.com>
2025-05-21 19:57:31 -0300

    Update consulta_09.sql

--- consultas/consulta_09.sql
+++ consultas/consulta_09.sql
@@ -10,7 +10,25 @@ m.NomeMunicipio Municipio,
 
 i91.IDH_Geral IndiceGeral_91,
 i00.IDH_Geral IndiceGeral_00,
-ROUND((i00.IDH_Geral - i91.IDH_Geral), 4) DiferencaIDH_Geral
+ROUND((i00.IDH_Geral - i91.IDH_Geral), 4) DiferencaIDH_Geral,
+
+-- IDH RENDA
+  
+i91.IDH_Renda IndiceRenda_91,
+i00.IDH_Renda IndiceRenda_00,
+ROUND((i00.IDH_Renda - i91.IDH_Renda), 4) DiferencaIDH_Renda,
+
+-- IDH LONGEVIDADE
+  
+i91.IDH_Longevidade IndiceLongevidade_91,
+i00.IDH_Longevidade IndiceLongevidade_00,
+ROUND((i00.IDH_Longevidade - i91.IDH_Longevidade), 4) DiferencaIDH_Longevidade,
+  
+-- IDH EDUCACAO
+  
+i91.IDH_Educacao IndiceEducacao_91,
+i00.IDH_Educacao IndiceEducacao_00,
+ROUND((i00.IDH_Educacao - i91.IDH_Educacao), 4) DiferencaIDH_Educacao
 
 FROM municipio m
 JOIN estado e
